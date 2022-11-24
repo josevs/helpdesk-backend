f3cc0a2bf7e4d2623e2eda73526b0771cac83081
Josevs <josevs.junior@gmail.com>
2022-11-24 18:41:15 -0300

    endpoint findById para Tecnico

--- src/main/java/com/valdir/helpdesk/domain/Tecnico.java
+++ src/main/java/com/valdir/helpdesk/domain/Tecnico.java
@@ -6,12 +6,14 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.valdir.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Tecnico extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
@@ -20,7 +22,7 @@ public class Tecnico extends Pessoa {
 		addPerfil(Perfil.TECNICO);
 	}
 
-	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+	public Tecnico(Long id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.TECNICO);
 	}
