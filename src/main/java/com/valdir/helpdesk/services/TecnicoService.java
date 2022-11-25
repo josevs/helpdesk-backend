364df681fab3304cef5fce9b2986d4cba8c93ab6
Josevs <josevs.junior@gmail.com>
2022-11-25 19:12:31 -0300

    endpoint create para Tecnico

--- src/main/java/com/valdir/helpdesk/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -1,11 +1,13 @@
 package com.valdir.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.valdir.helpdesk.domain.Tecnico;
+import com.valdir.helpdesk.domain.dtos.TecnicoDTO;
 import com.valdir.helpdesk.repositories.TecnicoRepository;
 import com.valdir.helpdesk.services.exceptions.ObjectnotfoundException;
 
@@ -20,4 +22,18 @@ public class TecnicoService {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotfoundException("Objeto não encontrado! Id: " + id));
 	}
+
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
+	
+	}
+
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null); //Para assegurar que o id será nulo.
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
+	}
+	
 }
